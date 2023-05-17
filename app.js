126dfca2757c42729843b45f63a75b0d141cafcd
Nermen <114686822+NermenElefky@users.noreply.github.com>
2023-05-17 04:19:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -21,3 +21,69 @@ searchBtn.addEventListener("click",()=>{
         searchIcon.style.color = "#fff"
     }
 })
+// let's make our slider in the landing page
+const wrapper = document.querySelector(".wrapper")
+let sliderWidth = document.querySelector(".firt-furniture-slide").clientWidth // this width will be changlable when making it responsive
+let bullets = document.querySelector(".bullets")
+let sliderNumber = 3
+let i = 3;
+
+// make slider work auto
+const sliderShow = function(){
+    let translatedValue = (sliderNumber - i)*(sliderWidth + 55)
+    wrapper.style.transform = `translateX(-${translatedValue}px)`
+    // remove the active class
+    bullets.querySelector(".active-slide").classList.remove("active-slide")
+    // now add it 
+    bullets.querySelector(`[data-slide-number = "${i}"]`).classList.add("active-slide")
+    i > 1 ? i-- : i=3 // to make the slider return to the first slide again
+    setTimeout(sliderShow , 7000)
+}
+sliderShow();
+
+// make slide go to next or prev slide
+
+let prev_x , active_slider_number , isDrag = false;
+const strat_drag = function(e){
+     isDrag = true // just to control mosuemove only work when start dragging
+     prev_x = e.pageX || e.touches[0].pageX
+     active_slider_number = bullets.querySelector(".active-slide").getAttribute("data-slide-number")
+}
+
+const prev_next_slide = function(e){
+        if (!isDrag) {return}
+        let current_x = e.pageX || e.touches[0].pageX
+        // check if the user wants the next or prev slider
+        if (prev_x > current_x){
+
+            if (active_slider_number == 1){
+                wrapper.style.transform = `translateX(0px)`
+            }
+            else{
+                let translatedValue = (active_slider_number)*(sliderWidth + 55)
+            wrapper.style.transform = `translateX(-${translatedValue}px)`
+            }
+        }
+        else{
+            if (active_slider_number  == sliderNumber){
+                let translatedValue = (active_slider_number-1)*(sliderWidth + 55)
+                wrapper.style.transform = `translateX(-${translatedValue}px)`
+            }
+            else{
+                let translatedValue = (active_slider_number-(sliderNumber-2))*(sliderWidth + 55)
+                wrapper.style.transform = `translateX(-${translatedValue}px)`
+            }
+        }
+} 
+
+const stop_drag = function (){
+    isDrag = false;
+}
+wrapper.addEventListener("mousedown",strat_drag)
+wrapper.addEventListener("touchstart",strat_drag)
+
+wrapper.addEventListener("mousemove",prev_next_slide)
+wrapper.addEventListener("touchmove",prev_next_slide)
+
+wrapper.addEventListener("mouseup",stop_drag)
+wrapper.addEventListener("touchend",stop_drag)
